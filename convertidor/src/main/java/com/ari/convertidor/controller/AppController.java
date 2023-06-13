fd89f9467a32493002aeaa34e27e6e2b6eaab727
kevincea503 <00203919@uca.edu.sv>
2023-06-13 00:33:49 -0600

    agrendo llave y delimitar

--- convertidor/src/main/java/com/ari/convertidor/controller/AppController.java
+++ convertidor/src/main/java/com/ari/convertidor/controller/AppController.java
@@ -1,5 +1,12 @@
 package com.ari.convertidor.controller;
+import com.ari.convertidor.model.ParametrosDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +22,23 @@ public class AppController {
 	    }
 
 	    @PostMapping("/convert")
-	    public String convertToJSON(@RequestBody String content) {
+	    public ResponseEntity<ParametrosDTO> convertToJSON(@RequestBody String requestBody) {
+	    	// Descomponer el objeto JSON recibido en un mapa de campos
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        Map<String, Object> jsonMap;
 	        try {
-	            ObjectMapper objectMapper = new ObjectMapper();
-	            Object json = objectMapper.readValue(content, Object.class);
-	            return objectMapper.writeValueAsString(json);
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            return null;
+	            jsonMap = objectMapper.readValue(requestBody, new TypeReference<Map<String, Object>>() {});
+	        } catch (IOException e) {
+	            return ResponseEntity.badRequest().build();
 	        }
+
+	        // Obtener los valores de los campos del objeto JSON
+	        String contenido = (String) jsonMap.get("content");
+	        String llave = (String) jsonMap.get("key");
+	        String delimitador = (String) jsonMap.get("delimit");
+
+	        // Combinar los valores en un objeto Person
+	        ParametrosDTO parametros = new ParametrosDTO(contenido, llave, delimitador);
+
+	        return ResponseEntity.ok(parametros);
 	    }}
